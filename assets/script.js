a1fa2a3427c59a9ca4d4447712422e566e281880
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-11-24 12:50:54 +0100

    script per nokia a buon punto creazione layout

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Telefono = /** @class */ (function () {
     function Telefono(_carica, _numerochiamate) {
         if (_carica === void 0) { _carica = 0; }
@@ -47,4 +62,70 @@ thirdUser.numero404();
 thirdUser.getNumeroChiamate();
 thirdUser.azzeraChiamate();
 thirdUser.getNumeroChiamate();
-/// fine esercizio base
+/////////////////////////// fine esercizio base/////////////////////////////////////////////////////////////
+// function dividereTempo(millisecondi: number): {
+//   ore: number;
+//   minuti: number;
+//   secondi: number;
+//   time: string;
+// } {
+//   // Calcolare le ore, i minuti, i secondi e i millisecondi
+//   const ore = Math.floor(millisecondi / 3600000);
+//   const minuti = Math.floor((millisecondi % 3600000) / 60000);
+//   const secondi = Math.floor((millisecondi % 60000) / 1000);
+//   let time = `${ore}:${minuti}:${secondi}`;
+//   return {
+//     ore,
+//     minuti,
+//     secondi,
+//     time
+//   };
+// }
+function durataChiamataSec(millisecondi) {
+    var durataSecondi = Math.floor((new Date().getTime() - millisecondi) / 1000);
+    return { durataSecondi: durataSecondi };
+}
+var nokia3320 = /** @class */ (function (_super) {
+    __extends(nokia3320, _super);
+    function nokia3320(_contatto, _carica, _numerochiamate, _intervalloChiamata, _tempoChiamata) {
+        if (_carica === void 0) { _carica = 0; }
+        if (_numerochiamate === void 0) { _numerochiamate = 0; }
+        if (_intervalloChiamata === void 0) { _intervalloChiamata = undefined; }
+        if (_tempoChiamata === void 0) { _tempoChiamata = undefined; }
+        var _this = _super.call(this, _carica, _numerochiamate) || this;
+        _this.contatto = _contatto;
+        _this.intervalloChiamata = _intervalloChiamata;
+        _this.tempoChiamata = _tempoChiamata;
+        return _this;
+    }
+    nokia3320.prototype.chiamata = function () {
+        var _this = this;
+        this.tempoChiamata = new Date().getTime();
+        this.intervalloChiamata = setInterval(function () { return console.log(durataChiamataSec(_this.tempoChiamata)); }, 1000);
+        this.numerochiamate++;
+    };
+    nokia3320.prototype.fineChiamata = function () {
+        if (this.tempoChiamata) {
+            var durataChiamata = durataChiamataSec(this.tempoChiamata).durataSecondi;
+            console.log(durataChiamata);
+            var costoChiamata = Number((durataChiamata * 0.0033333333).toFixed(2));
+            console.log(costoChiamata);
+            this.carica -= costoChiamata;
+            console.log("La chiamata appena terminata ti è costata " + costoChiamata + "$");
+            clearInterval(this.intervalloChiamata);
+            this.tempoChiamata = undefined;
+        }
+        else {
+            console.log("Inizia la chiamata prima di terminarla.");
+        }
+    };
+    return nokia3320;
+}(Telefono));
+var luca = new nokia3320("Luca Favaretto", 20);
+luca.ricarica(20);
+luca.chiamata();
+luca.fineChiamata();
+luca.numero404();
+luca.getNumeroChiamate();
+luca.azzeraChiamate();
+luca.getNumeroChiamate();
--- assets/script.ts
+++ assets/script.ts
@@ -50,4 +50,89 @@ thirdUser.getNumeroChiamate();
 thirdUser.azzeraChiamate();
 thirdUser.getNumeroChiamate();
 
-/// fine esercizio base
+/////////////////////////// fine esercizio base/////////////////////////////////////////////////////////////
+// function dividereTempo(millisecondi: number): {
+//   ore: number;
+//   minuti: number;
+//   secondi: number;
+//   time: string;
+// } {
+//   // Calcolare le ore, i minuti, i secondi e i millisecondi
+//   const ore = Math.floor(millisecondi / 3600000);
+//   const minuti = Math.floor((millisecondi % 3600000) / 60000);
+//   const secondi = Math.floor((millisecondi % 60000) / 1000);
+
+//   let time = `${ore}:${minuti}:${secondi}`;
+
+//   return {
+//     ore,
+//     minuti,
+//     secondi,
+//     time
+//   };
+// }
+function durataChiamataSec(millisecondi: number): {
+  durataSecondi: number;
+} {
+  const durataSecondi = Math.floor(
+    (new Date().getTime() - millisecondi) / 1000
+  );
+  return { durataSecondi };
+}
+
+class nokia3320 extends Telefono {
+  contatto: string;
+  tempoChiamata: number | undefined;
+  intervalloChiamata: number | undefined;
+  constructor(
+    _contatto: string,
+    _carica: number = 0,
+    _numerochiamate: number = 0,
+    _intervalloChiamata: number | undefined = undefined,
+    _tempoChiamata: number | undefined = undefined
+  ) {
+    super(_carica, _numerochiamate);
+    this.contatto = _contatto;
+    this.intervalloChiamata = _intervalloChiamata;
+    this.tempoChiamata = _tempoChiamata;
+  }
+  public chiamata(): void {
+    this.tempoChiamata = new Date().getTime();
+    this.intervalloChiamata = setInterval(
+      () => console.log(durataChiamataSec(this.tempoChiamata!)),
+      1000
+    );
+
+    this.numerochiamate++;
+  }
+  public fineChiamata(): void {
+    if (this.tempoChiamata) {
+      let durataChiamata: number = durataChiamataSec(
+        this.tempoChiamata
+      ).durataSecondi;
+      console.log(durataChiamata);
+      let costoChiamata = Number((durataChiamata * 0.0033333333).toFixed(2));
+      console.log(costoChiamata);
+      this.carica -= costoChiamata;
+
+      console.log(
+        "La chiamata appena terminata ti è costata " + costoChiamata + "$"
+      );
+      clearInterval(this.intervalloChiamata);
+
+      this.tempoChiamata = undefined;
+    } else {
+      console.log("Inizia la chiamata prima di terminarla.");
+    }
+  }
+}
+
+let luca = new nokia3320("Luca Favaretto", 20);
+
+luca.ricarica(20);
+luca.chiamata();
+luca.fineChiamata();
+luca.numero404();
+luca.getNumeroChiamate();
+luca.azzeraChiamate();
+luca.getNumeroChiamate();
